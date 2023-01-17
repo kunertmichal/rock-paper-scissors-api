a5bb7ecd23a04fa9147ef4f167f9f0cfda29a6fe
Michal Kunert <30593515+kunertmichal@users.noreply.github.com>
2023-01-17 16:58:57 +0100

    add game components

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,34 +1,74 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { GameService } from './game.service';
-import { CreateGameDto } from './dto/create-game.dto';
-import { UpdateGameDto } from './dto/update-game.dto';
 
-@WebSocketGateway()
-export class GameGateway {
-  constructor(private readonly gameService: GameService) {}
+@WebSocketGateway({ cors: { origin: '*' } })
+export class GameGateway implements OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
 
-  @SubscribeMessage('createGame')
-  create(@MessageBody() createGameDto: CreateGameDto) {
-    return this.gameService.create(createGameDto);
+  constructor(readonly gameService: GameService) {}
+
+  @SubscribeMessage('game:create')
+  createRoom(@MessageBody() data: { clientId: string }) {
+    const gameId = this.gameService.create();
+    this.server.to(data.clientId).emit('game:created', gameId);
   }
 
-  @SubscribeMessage('findAllGame')
-  findAll() {
-    return this.gameService.findAll();
+  @SubscribeMessage('game:player-joined')
+  joinRoom(@MessageBody() data: { gameId: string; clientId: string }) {
+    if (this.gameService.getNumberOfPlayers(data.gameId) === 2) {
+      return this.server.to(data.clientId).emit('game:full');
+    }
+    this.gameService.addPlayerToGame(data.gameId, data.clientId);
+    this.server.socketsJoin(data.gameId);
+    if (this.gameService.getNumberOfPlayers(data.gameId) > 1) {
+      return this.server
+        .to(data.gameId)
+        .emit('game:started', this.gameService.getScore(data.gameId));
+    }
+    this.server.to(data.gameId).emit('game:waiting-for-player');
   }
 
-  @SubscribeMessage('findOneGame')
-  findOne(@MessageBody() id: number) {
-    return this.gameService.findOne(id);
+  @SubscribeMessage('game:choice')
+  choice(
+    @MessageBody()
+    data: {
+      gameId: string;
+      clientId: string;
+      choice: 'rock' | 'paper' | 'scissors';
+    },
+  ) {
+    this.gameService.addChoice(data.gameId, data.clientId, data.choice);
+    const winner = this.gameService.determineWinner(data.gameId);
+    const score = this.gameService.getScore(data.gameId);
+    if (winner) {
+      this.server.to(data.gameId).emit('game:over', {
+        winner,
+        score,
+      });
+    }
+    const rivalId = this.gameService.getRivalId(data.gameId, data.clientId);
+    this.server.to(rivalId).emit('game:waiting-for-move');
   }
 
-  @SubscribeMessage('updateGame')
-  update(@MessageBody() updateGameDto: UpdateGameDto) {
-    return this.gameService.update(updateGameDto.id, updateGameDto);
+  @SubscribeMessage('game:play-again')
+  restart(@MessageBody() data: { gameId: string }) {
+    this.gameService.resetChoices(data.gameId);
+    this.server
+      .to(data.gameId)
+      .emit('game:started', this.gameService.getScore(data.gameId));
   }
 
-  @SubscribeMessage('removeGame')
-  remove(@MessageBody() id: number) {
-    return this.gameService.remove(id);
+  handleDisconnect(client: Socket) {
+    const gameId = this.gameService.getGameIdFromClientId(client.id);
+    this.gameService.removePlayerFromGame(client.id, gameId);
+    this.server.to(gameId).emit('game:waiting-for-player');
   }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,26 +1,122 @@
 import { Injectable } from '@nestjs/common';
-import { CreateGameDto } from './dto/create-game.dto';
-import { UpdateGameDto } from './dto/update-game.dto';
+import { nanoid } from 'nanoid';
+
+interface Choice {
+  clientId: string;
+  choice: 'rock' | 'paper' | 'scissors';
+}
+
+type Game = {
+  [key: string]: {
+    players: string[];
+    choices: Choice[];
+    score: {
+      [key: string]: number;
+    };
+  };
+};
 
 @Injectable()
 export class GameService {
-  create(createGameDto: CreateGameDto) {
-    return 'This action adds a new game';
+  games: Game = {};
+
+  create() {
+    const gameId = nanoid(10);
+    this.games[gameId] = { players: [], choices: [], score: {} };
+    return { gameId };
   }
 
-  findAll() {
-    return `This action returns all game`;
+  read(gameId: string) {
+    if (!(gameId in this.games)) {
+      return null;
+    }
+    return this.games[gameId];
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} game`;
+  getGameIdFromClientId(clientId: string) {
+    return Object.keys(this.games).find((gameId) =>
+      this.games[gameId].players.includes(clientId),
+    );
+  }
+  removePlayerFromGame(clientId: string, gameId: string) {
+    this.games[gameId].players = this.games[gameId].players.filter(
+      (player) => player !== clientId,
+    );
+    if (this.games[gameId].players.length === 0) {
+      delete this.games[gameId];
+    }
   }
 
-  update(id: number, updateGameDto: UpdateGameDto) {
-    return `This action updates a #${id} game`;
+  getScore(gameId: string) {
+    return this.games[gameId].score;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} game`;
+  getNumberOfPlayers(gameId: string) {
+    return this.games[gameId]?.players.length;
+  }
+
+  addPlayerToGame(gameId: string, clientId: string) {
+    this.games[gameId].players.push(clientId);
+    if (this.games[gameId].players.length === 2) {
+      this.initializeScore(gameId);
+    }
+  }
+
+  initializeScore(gameId: string) {
+    return (this.games[gameId].score = {
+      [this.games[gameId].players[0]]: 0,
+      [this.games[gameId].players[1]]: 0,
+    });
+  }
+
+  addChoice(
+    gameId: string,
+    clientId: string,
+    choice: 'rock' | 'paper' | 'scissors',
+  ) {
+    if (this.games[gameId].choices.length === 2) {
+      throw new Error('Cannot add more choices');
+    }
+    this.games[gameId].choices.push({ clientId, choice });
+  }
+
+  resetChoices(gameId: string) {
+    this.games[gameId].choices = [];
+  }
+
+  getRivalId(gameId: string, clientId: string) {
+    return this.games[gameId].players[0] === clientId
+      ? this.games[gameId].players[1]
+      : this.games[gameId].players[0];
+  }
+
+  determineWinner(gameId: string) {
+    const { choices } = this.games[gameId];
+    if (choices.length !== 2) {
+      return null;
+    }
+    const possibleChoices = ['rock', 'paper', 'scissors'] as const;
+    const player1 = choices[0];
+    const player2 = choices[1];
+
+    if (player1.choice === player2.choice) {
+      return { winner: 'tie' };
+    }
+    if (
+      possibleChoices.indexOf(player1.choice) ===
+      (possibleChoices.indexOf(player2.choice) + 1) % 3
+    ) {
+      this.games[gameId].score[player1.clientId] += 1;
+      return {
+        winner: player1.clientId,
+        score: this.games[gameId].score,
+      };
+    } else {
+      this.games[gameId].score[player2.clientId] += 1;
+      return {
+        winner: player2.clientId,
+        score: this.games[gameId].score,
+      };
+    }
   }
 }
